2a59b1b17cdd2dec02eb65df44e8216ad6d8ba4d
tatyu <tatyu@t.local>
2024-07-01 15:57:20 +0900

    :seedling:feat: ex05

---
+++ ex05/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"piscine"
+)
+
+func main() {
+	// fmt.Println(piscine.Sqrt(4))
+	// fmt.Println(piscine.Sqrt(3))
+	fmt.Println(piscine.Sqrt(9223372030926249001))
+}
---
+++ ex05/test/sqrt_test.go
@@ -0,0 +1,22 @@
+package test
+
+import (
+	"piscine"
+	"testing"
+)
+
+func expect(actual, expect int, t *testing.T) {
+	if actual != expect {
+		t.Errorf("expected %d, but got %d", expect, actual)
+	}
+}
+
+func TestIterativeFactorial(t *testing.T) {
+	expect(piscine.Sqrt(0), 0, t)
+	expect(piscine.Sqrt(1), 0, t)
+	expect(piscine.Sqrt(-3), 0, t)
+	expect(piscine.Sqrt(2), 0, t)
+	expect(piscine.Sqrt(4), 2, t)
+	expect(piscine.Sqrt(9223372030926249001), 3037000499, t)
+	expect(piscine.Sqrt(9223372030926249002), 0, t)
+}
